08dcf66bb9b776413c9959d06e291cbc89d67aa0
Harsh <74773161+FakePickle@users.noreply.github.com>
2023-01-24 08:25:49 +0530

    Changes to be committed: 	new file:   Assignment 3.pdf 	new file:   Q1.py 	new file:   Q2.py 	new file:   data.txt

---
+++ Q1.py
@@ -0,0 +1,21 @@
+#printing the top half of the diamond using recursion
+def top_half(n,count = 0):
+    if n==1:
+        print('* '*n + ' '*(count*2) + '* '*n)
+    else:
+        print('* '*n + ' '*(count*2) + '* '*n)
+        top_half(n-1,count+2)
+
+#printing the bottom half of the diamond using recursion
+def bottom_half(n,count = 2):
+    if n == 2:
+        print('* '*count + ' '*((n-2)*4) + '* '*count)
+    else:
+        print('* '*count + ' '*((n-2)*4) + '* '*count)
+        bottom_half(n-1,count+1)
+
+#drivers code
+if __name__ == '__main__':
+    n = int(input("Enter the size of the diamond : "))
+    top_half(n)
+    bottom_half(n)
---
+++ Q2.py
@@ -0,0 +1,74 @@
+def main():
+    print('Welcome to the Off_campus Program\n'
+    '1. Getting data by inputting student name and whether he is in the campus or not\n'
+    '2. Getting all the students who entered the campus during a certain that is inputted by the user\n'
+    '3. Getting all the students who entered through the gate from the specific gate number\n'
+    '4. Exit')
+    temp_dict = {}
+    Data = []
+    creating_dictionary(temp_dict,Data)
+    while True:
+        user_input = int(input('Please enter you choice : '))
+        if(user_input==1):
+            finding_data_by_name(Data)
+        elif(user_input==2):
+            finding_data_by_time(Data)
+        elif(user_input==3):
+            finding_data_by_gate(Data)
+        elif(user_input==4):
+            break
+        else:
+            print("Invalid Input")
+
+class Off_campus:
+    def __init__(self,name,crossing,Gate_number,Time):
+        self.name = name
+        self.cross = crossing
+        self.gate = Gate_number
+        self.time = Time
+    
+    def dictionary(self,day_scholar_cross):
+        temp_dict = {}
+        temp_dict[self.name] = [{
+            'Crossing': self.cross,
+            'Gate Number': self.gate,
+            'Time': self.time
+        }]
+        if day_scholar_cross == {}:
+            day_scholar_cross = temp_dict
+        else:
+            for k,v in day_scholar_cross.items():
+                temp_list = []
+                if self.name == k:
+                    temp_list.extend(v)
+                    temp_list.extend(temp_dict[self.name])
+                    day_scholar_cross[self.name] = temp_list
+                else:
+                    day_scholar_cross = temp_dict
+        return day_scholar_cross
+
+def creating_dictionary(dictionary,data_list):
+    with open('data.txt') as infile:
+        line_split = infile.read().splitlines()
+        for i in range(1,len(line_split)):
+            line_split[i] = line_split[i].split(', ')
+            comma_split = Off_campus(line_split[i][0],line_split[i][1],line_split[i][2],line_split[i][3])
+            temp_list = [comma_split.dictionary(dictionary)]
+            data_list.extend(temp_list)
+
+def finding_data_by_name(data_list):
+    user_input = input()
+    for i in data_list:
+        for k,v in i.items():
+            if k==user_input:
+                for j in v:
+                    if j['Time']>='06:00:00' and j['Time']<='19:00:00':
+                        print(i)
+
+def finding_data_by_time(data_list):
+    a = 1
+
+def finding_data_by_gate(data_list):
+    a = 1
+
+main()
